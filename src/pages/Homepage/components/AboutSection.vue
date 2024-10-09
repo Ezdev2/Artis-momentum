16f0883c7360369034b872c97824381be9f3caf4
ezra.fa <ezra.fa@centraltest.com>
2024-10-09 15:57:11 +0200

    add ressource section

--- src/pages/Homepage/components/AboutSection.vue
+++ src/pages/Homepage/components/AboutSection.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="py-[122px] px-[24px] sm:px-[122px] bg-secondary flex flex-col gap-6 justify-center items-cente">
+  <div class="py-[122px] px-[24px] sm:px-[122px] bg-secondary flex flex-col gap-6 justify-center">
     <div class="flex flex-col gap-2">
-      <p class="font-bold text-primary uppercase">Qui sommes-nous ?
+      <p class="font-bold text-blackScale uppercase">Qui sommes-nous ?
       </p>
       <h2 class="font-bold text-primary">A propos de artis Momentum, le spécialiste de la formation des commerciaux
       </h2>
@@ -9,17 +9,32 @@
 
     <div>
       <div class="box-area">
-        <div class="box" v-for="(item, index) in boxes" :key="index" :class="{ active: activeIndex === index }"
+        <div class="box flex gap-2"
+          :style="{ backgroundImage: 'url(' + item.image + ')', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }"
+          v-for="(item, index) in boxes" :key="index" :class="{ active: activeIndex === index }"
           @click="setActive(index)">
-          <img :src="item.image" alt="img" />
-          <!-- <i :class="['fa-solid', item.iconClass]"></i> -->
-           <div class="icon">
-             <Icon class="h-6 w-6" :icon="item.icon" />
-           </div>
-          <h2>{{ item.title }}</h2>
-          <p>{{ item.description }}</p>
+          <!-- <img :src="item.image" alt="img" /> -->
+          <div class="absolute bottom-2 flex items-end gap-4 p-4">
+            <div class="icon">
+              <Icon class="h-6 w-6" :icon="item.icon" />
+            </div>
+            <div class="flex flex-col">
+              <h2 class="text-left">{{ item.title }}</h2>
+              <p class="text-left w-[300px] w-[300px] md:w-[600px] lg:w-[800px] overflow-hidden text-ellipsis whitespace-nowrap">{{
+                item.description }}</p>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+
+    <div class="w-full flex flex-col items-center">
+      <button
+        class="w-full md:max-w-[200px] flex gap-4 justify-center items-center text-center px-6 py-3.5 text-white bg-primary hover:bg-secondary"
+        @click="goTo('/')">
+        <span>En savoir plus</span>
+        <img width="12" src="../../../assets/arrow.svg" alt="">
+      </button>
     </div>
   </div>
 
@@ -34,28 +49,28 @@ const activeIndex = ref(0);
 // Boxes data
 const boxes = [
   {
-    image: 'https://images.pexels.com/photos/2837909/pexels-photo-2837909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/15149190/pexels-photo-15149190/free-photo-of-apprendre-le-volley-ball.jpeg?auto=compress&cs=tinysrgb&w=2400&h=1200&dpr=1',
     icon: "mdi:company",
-    title: 'Las Vegas',
-    description: 'Lorem ipsum dolor sit amet.'
+    title: 'Recrutement',
+    description: "Depuis 15 ans, nos +100 recruteurs trouvent les meilleurs potentiels partout en France et dans tous les secteurs d'activité. Fonctions commerciales, marketing et executive search."
   },
   {
-    image: 'https://images.pexels.com/photos/922978/pexels-photo-922978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/4144923/pexels-photo-4144923.jpeg?auto=compress&cs=tinysrgb&w=2400&h=1200&dpr=1',
     icon: 'mingcute:house-line',
-    title: 'Switzerland',
-    description: 'Lorem ipsum dolor sit amet.'
+    title: 'Formation',
+    description: "Artis Momentum est l'organisme de formation spécialisé dans la vente. Améliorez les performances commerciales de votre équipe grâce à 14 programmes pensés et délivrés par des pros de la vente."
   },
   {
-    image: 'https://images.pexels.com/photos/3723034/pexels-photo-3723034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/4101143/pexels-photo-4101143.jpeg?auto=compress&cs=tinysrgb&w=2400&h=1200&dpr=1',
     icon: 'heroicons:fire',
-    title: 'Amazon Rainforest',
-    description: 'Lorem ipsum dolor sit amet.'
+    title: 'Coaching Commercial',
+    description: "Révélez toute la puissance de votre équipe commerciale : coacher vos managers et vos commerciaux est le meilleur moyen de maximiser leur productivité et leur motivation ! Découvrez notre méthode de coaching."
   },
   {
-    image: 'https://images.pexels.com/photos/10983409/pexels-photo-10983409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&w=2400&h=1200&dpr=1',
     icon: 'uil:user',
-    title: 'Siberia',
-    description: 'Lorem ipsum dolor sit amet.'
+    title: 'Conseil',
+    description: "Uptoo est le 1er cabinet de transformation commerciale. Nos consultants vous aident à construire la bonne méthode de vente qui allie productivité et efficacité."
   }
 ];
 
@@ -63,24 +78,30 @@ const boxes = [
 function setActive(index) {
   activeIndex.value = index;
 }
+
+function goTo(link) {
+  console.log(link);
+}
 </script>
 
 <style lang="scss" scoped>
 .box-area {
   width: 100%;
-  display: flex;
+  height: 620px;
   justify-content: center;
   align-items: center;
+  @apply flex flex-col md:flex-row gap-4 md:gap-0;
 }
 
 .box {
-  width: 180px;
+  // width: 120px;
   height: 600px;
-  margin-right: 20px;
+  // margin-right: 20px;
+  @apply w-1/2 md:w-[120px] md:mr-[20px];
   overflow: hidden;
   border-radius: 20px;
   position: relative;
-  transition: all linear 0.6s;
+  transition: all linear 1s;
 }
 
 .box:last-child {
@@ -105,40 +126,18 @@ function setActive(index) {
 
 .box {
   .icon {
-    width: 40px;
+    min-width: 40px;
     height: 40px;
     border-radius: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
     background: #fff;
-    position: absolute;
-    bottom: 20px;
-    left: 10px;
     font-size: 20px;
   }
 }
 
-.box:nth-child(1) i {
-  color: #ff5200;
-}
-
-.box:nth-child(2) i {
-  color: #ca470a;
-}
-
-.box:nth-child(3) i {
-  color: #00d9ff;
-}
-
-.box:nth-child(4) i {
-  color: #0077ff;
-}
-
 .box h2 {
-  position: absolute;
-  bottom: 30px;
-  left: 60px;
   font-family: 'Poppins', sans-serif;
   font-size: 25px;
   color: #f6f6f6;
@@ -148,9 +147,6 @@ function setActive(index) {
 }
 
 .box p {
-  position: absolute;
-  bottom: 10px;
-  left: 60px;
   font-family: 'Poppins', sans-serif;
   font-size: 18px;
   font-weight: 400;
@@ -168,7 +164,7 @@ function setActive(index) {
 }
 
 .active {
-  width: 500px;
-  transition: all linear 0.6s;
+  @apply w-full md:w-full;
+  transition: all linear 1s;
 }
 </style>
--- src/pages/Homepage/components/RessourceSection.vue
+++ src/pages/Homepage/components/RessourceSection.vue
@@ -1,12 +1,109 @@
 <template>
-    <div class="h-[100vh] bg-primary flex justify-center items-center">
-        <h1 class="text-center text-white font-bold">Nos ressources
-        </h1>
-    </div>
+    <div class="py-[122px] px-[24px] sm:px-[122px] bg-primary flex flex-col gap-6 justify-center items-center">
+        <div class="flex flex-col gap-2 w-full">
+            <p class="font-bold text-secondary text-left uppercase">Nos ressources</p>
+            <h2 class="font-bold text-left text-white">Vive la vente !</h2>
+            <p class="text-white text-left">
+                Trouvez la bonne formation, adaptée à votre besoin, votre secteur
+                d'activité et votre métier : Commercial(e) terrain, Key Account Manager (KAM), Directeur(trice)
+                Commercial(e)...
+            </p>
+        </div>
 
+        <!-- Infinite scroll slider -->
+        <div class="overflow-hidden relative w-full">
+            <div class="slide-track">
+                <div class="w-[300px] mx-[10px] flex justify-center" v-for="n in 14" :key="n">
+                    <div class="card bg-white rounded-[10px] py-[12px] px-[20px] flex items-center h-[100%]">
+                        <div class="w-full h-[120px] flex items-center justify-center">
+                            <img width="84" :src="getImageSrc(n)" alt="" />
+                        </div>
+                        <div class="flex py-[20px] text-center">
+                            <div class="flex flex-col gap-2 items-start">
+                                <h4 class="font-bold text-left ">{{ getCardTitle(n) }}</h4>
+                                <p class="text-left font-thin">{{ getCardDescription(n) }}</p>
+                            </div>
+                            <a href="#" class="card-button">
+                                <img src="../../../assets/arrow-black.svg" alt="">
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
+<script setup>
+import book from '../../../assets/book.svg';
+import article from '../../../assets/article.svg';
+import audio from '../../../assets/audio.svg';
+import liveclass from '../../../assets/liveclass.svg';
+import newsletter from '../../../assets/newsletter.svg';
+import video from '../../../assets/video.svg';
+
+const cards = [
+    { image: book, title: 'Livre', description: 'Découvrez notre sélection de livres spécialisés.' },
+    { image: article, title: 'Article', description: 'Lisez des articles experts sur les ventes.' },
+    { image: audio, title: 'Podcast', description: 'Écoutez nos podcasts pour des conseils pratiques.' },
+    { image: liveclass, title: 'Live Class', description: 'Participez à nos formations en direct.' },
+    { image: newsletter, title: 'Newsletter', description: 'Inscrivez-vous à notre newsletter.' },
+    { image: video, title: 'Vidéo', description: 'Regardez nos vidéos de formations.' }
+];
+const getImageSrc = (index) => {
+    return cards[(index - 1) % cards.length].image;
+};
+
+const getCardTitle = (index) => {
+    return cards[(index - 1) % cards.length].title;
+};
+
+const getCardDescription = (index) => {
+    return cards[(index - 1) % cards.length].description;
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@keyframes scroll {
+    0% {
+        transform: translateX(0);
+    }
+
+    100% {
+        transform: translateX(calc(-300px * 7));
+    }
+}
+
+.slide-track {
+    animation: scroll 40s linear infinite;
+    display: flex;
+    width: calc(300px * 14);
+}
+
+.card {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease-in-out;
+}
+
+.card:hover {
+    transform: scale(1.05);
+}
+
+.card-button {
+    display: inline-block;
+    padding: 10px 20px;
+    // background-color: var(--color-secondary);
+    // color: var(--color-primary);
+    // text-decoration: none;
+    border-radius: 5px;
+    font-size: 14px;
+    transition: background-color 0.3s;
+}
+
+.card-button:hover {
+    // background-color: var(--color-primary);
+    border: 1px solid var(--color-primary);
+    // color: var(--color-secondary);
+}
+</style>
